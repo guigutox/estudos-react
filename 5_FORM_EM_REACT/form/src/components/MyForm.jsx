94ccbecf02551369a3e0872b7b170a3ead8db20b
Guilherme <guilhermegutofer@gmail.com>
2025-05-02 13:48:03 -0300

    Se aplicado, esse commit ira adicionar estaudos relacionados a manipulacao de valores dos inputs

--- 5_FORM_EM_REACT/form/src/components/MyForm.jsx
+++ 5_FORM_EM_REACT/form/src/components/MyForm.jsx
@@ -1,17 +1,33 @@
 import './MyForm.css'
 
+import {useState} from 'react'
+
 const MyForm = () => {
+
+  //3- gerenciamento de dados
+
+  const [name, setName] = useState()
+  const [email, setEmail] = useState()
+
+  const handleName = (e) =>{
+    setName(e.target.value)
+  }
+
+  console.log(name);
+  console.log(email);
+
+
   return (
     <div>   
     {/*1- Criacao de form*/}
     <form>
         <div>
             <label htmlFor="name">Nome:</label>
-            <input type="text" name="name" placeholder='Digite o seu nome'/>
+            <input type="text" name="name" placeholder='Digite o seu nome'onChange={handleName}/>
         </div>
         <label>
           <span>E-mail:</span>
-          <input type="email" name="email" placeholder="Digite o seu e-mail"/>
+          <input type="email" name="email" placeholder="Digite o seu e-mail" onChange={(e) => setEmail(e.target.value) }/>
         </label>
 
         <input type="submit" value="Enviar" />
